f461ef670ada5b232324d63add9b28ab93b85162
IncredibleQuark <lkupinski@gmail.com>
2018-04-08 00:27:17 +0200

    custom material theme

--- client/src/app/app-material.module.ts
+++ client/src/app/app-material.module.ts
@@ -1,9 +1,12 @@
 
-import {MatCardModule, MatFormFieldModule, MatInputModule, MatTabsModule} from '@angular/material';
+import {
+  MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatProgressSpinnerModule,
+  MatTabsModule
+} from '@angular/material';
 import {NgModule} from '@angular/core';
 
 @NgModule({
-    imports: [MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule],
-    exports: [MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule],
+    imports: [MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatProgressSpinnerModule, MatIconModule],
+    exports: [MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatProgressSpinnerModule, MatIconModule],
 })
 export class MaterialModule { }
