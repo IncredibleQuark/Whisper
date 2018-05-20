4e3264eef1f6ccfdc7d822995b4eb44b0052eab3
IncredibleQuark <lkupinski@gmail.com>
2018-05-20 18:59:46 +0200

    messages scroll done

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {ChatService} from '../../services/chat/chat.service';
 import {AuthService} from '../../services/auth/auth.service';
+import {MatListItem} from "@angular/material";
 
 @Component({
   selector: 'app-chat',
@@ -11,10 +12,12 @@ import {AuthService} from '../../services/auth/auth.service';
 export class ChatComponent implements OnInit {
 
   messages: string[] = [];
+  @ViewChildren(MatListItem, {read: ElementRef}) matListItems: QueryList<MatListItem>;
+  @ViewChild('messagesBox', {read: ElementRef}) messagesBox: ElementRef;
 
   constructor(private chatService: ChatService, private authService: AuthService) {
 
-    this.authService.getProfile().subscribe( profile => {
+    this.authService.getProfile().subscribe(profile => {
 
       this.chatService.logUser(profile['user']['username']);
     });
@@ -23,9 +26,17 @@ export class ChatComponent implements OnInit {
 
   ngOnInit() {
 
-    this.chatService.getMessages().subscribe((message: string) => {
+    this.chatService.getMessages().subscribe((message) => {
         this.messages.push(message);
-      });
+      }, err => console.warn(err)
+    )
+  }
+
+  ngAfterViewInit(): void {
+    // subscribing to any changes in the list of messages
+    this.matListItems.changes.subscribe(() => {
+      this.scrollToBottom();
+    });
   }
 
   send(messageForm) {
@@ -35,6 +46,12 @@ export class ChatComponent implements OnInit {
       this.chatService.sendMessage(data);
     }
 
+  }
+
+  scrollToBottom() {
+    try {
+         this.messagesBox.nativeElement.scrollTop = this.messagesBox.nativeElement.scrollHeight;
+    } catch {}
   }
 
 }
