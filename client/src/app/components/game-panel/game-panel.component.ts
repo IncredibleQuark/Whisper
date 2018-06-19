d0c930db35d02e2069822c52e119fc171aaf34f1
IncredibleQuark <lkupinski@gmail.com>
2018-06-19 23:53:33 +0200

    created game timer functions

--- client/src/app/components/game-panel/game-panel.component.ts
+++ client/src/app/components/game-panel/game-panel.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {GameService} from "../../services/game/game.service";
 import {AuthService} from "../../services/auth/auth.service";
+import {Subscription} from "rxjs/Subscription";
+import {TimerObservable} from "rxjs/observable/TimerObservable";
 
 @Component({
   selector: 'app-game-panel',
@@ -15,24 +17,27 @@ export class GamePanelComponent implements OnInit {
   isDrawing: boolean;
   gameStarted: boolean;
   user: any;
+  time: Date;
+  timeObservable: Subscription;
 
   constructor(private gameService: GameService, private authService: AuthService) {
 
     this.isReady = false;
     this.allReady = false;
-
     this.isDrawing = true; //TODO create a queue
+    this.time = new Date('2018-01-01 00:02:00');
 
     this.gameService.gameStatus().subscribe((slogan: string) => {
       this.slogan = slogan;
       this.gameStarted = true;
+      this.startTimer();
     });
 
-    this.authService.getProfile().subscribe( (user: any) => {
+    this.authService.getProfile().subscribe((user: any) => {
       this.user = user.user;
     });
 
-    this.gameService.allReady().subscribe( (data: boolean) => {
+    this.gameService.allReady().subscribe((data: boolean) => {
       this.allReady = data;
     })
   }
@@ -48,6 +53,31 @@ export class GamePanelComponent implements OnInit {
 
   startGame() {
     this.gameService.startGame();
+  }
+
+  ngOnDestroy() {
+    this.stopTimer();
+  }
+
+  private startTimer() {
+    let tick = TimerObservable.create(1000, 1000);
+
+    this.timeObservable = tick.subscribe((t: number) => {
+
+      if (this.time.getMinutes() === 0 && this.time.getSeconds() === 0) {
+        console.warn('time is up');
+        this.stopTimer();
+        return false;
+        //TODO handle sockets
+      }
+
+      this.time = new Date(this.time.getTime() - 1000);
+    });
+
+  }
+
+  private stopTimer() {
+    this.timeObservable.unsubscribe();
   }
 
 }
