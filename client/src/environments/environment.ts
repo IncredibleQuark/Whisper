da866444973220fdc76c7caffe5e6bdac9b0f53e
IncredibleQuark <lkupinski@gmail.com>
2018-08-29 22:17:47 +0200

    dev env fix, socket ssl test

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/',
+  apiUrl: 'http://localhost:8080/api/',
   socketUrl: 'http://localhost:3001'
 };
--- server/index.js
+++ server/index.js
@@ -14,16 +14,17 @@ const userRoutes = require('./routes/user/userRoutes');
 const sloganRoutes = require('./routes/slogan/sloganRoutes');
 const server = http.Server(app);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 const socketPort = 3001;
 const sockets = require('./sockets/main');
 
-const sslPort = 8080;
-const sslOptions = {
-  key: fs.readFileSync('/etc/letsencrypt/live/pictionary.pl/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/pictionary.pl/fullchain.pem')
-}
-const secureServer = https.createServer(sslOptions, app);
+
+//Define your ssl certificate paths
+// const sslOptions = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/pictionary.pl/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/pictionary.pl/fullchain.pem')
+// }
+// const secureServer = https.createServer(sslOptions, app);
 
 // Connect to database
 mongoose.connect(config.database).then( () => {
@@ -36,28 +37,35 @@ app.use(cors());
 
 // Set Static Folder
 // app.use(express.static(path.join(__dirname, '../client/dist')));
+// Uncomment if you want to serve app from node
+
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 require('./config/passport')(passport);
 
-
 app.use('/api/user', userRoutes);
 app.use('/api/slogan', sloganRoutes);
 app.get('/', (req, res) => {res.send('Invalid Endpoint');});
+
 // app.get('*', (req, res) => {res.sendFile(path.join(__dirname, '../client/dist/index.html'));});
+// Uncomment if you want to serve app from node
 
 
+//If you don't have SSL on prod and good for development use
 server.listen(socketPort, () => {
     console.warn(`Listen sockets on: ${socketPort}`);
 });
-
 sockets.init(server);
-
 app.listen(port, () => {
   console.log(`App started on port: ${port}`);
 });
 
-secureServer.listen(sslPort, () => {
-  console.log(`Ssl listen on: ${sslPort}`);
-})
+//If you have configured SSL on prod
+// secureServer.listen(socketPort, () => {
+//   console.warn(`Listen sockets on: ${socketPort}`);
+// })
+// sockets.init(secureServer);
+// secureServer.listen(port, () => {
+//   console.log(`Ssl listen on: ${sslPort}`);
+// })
